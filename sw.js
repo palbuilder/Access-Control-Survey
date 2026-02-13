131285bc70ab88ddc29b080f8189d0a88f531ed1
palbuilder <brandinkaeser@hotmail.com>
2026-02-13 16:10:56 -0500

    Add files via upload
    Files from app v2

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-const CACHE_NAME = "acs_cache_v1";
+const CACHE_NAME = "pal_acs_cache_v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./sw.js",
   "./icon_192.png",
-  "./icon_512.png"
+  "./icon_512.png",
+  "./pal_logo.png",
+  "./handing_ref.jpg",
+  "./single_door_measurements.jpg",
+  "./double_door_measurements.jpg",
+  "./frame_ref_1.jpg",
+  "./frame_ref_2.jpg"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
@@ -25,11 +30,13 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       if (cached) return cached;
-      return fetch(event.request).then((resp) => {
-        const copy = resp.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return resp;
-      }).catch(() => caches.match("./index.html"));
+      return fetch(event.request)
+        .then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return resp;
+        })
+        .catch(() => caches.match("./index.html"));
     })
   );
 });
