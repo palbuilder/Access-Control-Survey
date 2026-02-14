347c64284857e3c1a705368fd4615eaed7dc30c5
palbuilder <brandinkaeser@hotmail.com>
2026-02-13 19:41:52 -0500

    Add files via upload
    V3 upload

--- sw.js
+++ sw.js
@@ -1,17 +1,15 @@
-const CACHE_NAME = "pal_acs_cache_v2";
+const CACHE_NAME = "pal_acs_cache_v3";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./sw.js",
   "./icon_192.png",
   "./icon_512.png",
   "./pal_logo.png",
   "./handing_ref.jpg",
   "./single_door_measurements.jpg",
   "./double_door_measurements.jpg",
-  "./frame_ref_1.jpg",
-  "./frame_ref_2.jpg"
+  "./frame_ref_1.jpg"
 ];
 
 self.addEventListener("install", (event) => {
@@ -30,13 +28,11 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       if (cached) return cached;
-      return fetch(event.request)
-        .then((resp) => {
-          const copy = resp.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return resp;
-        })
-        .catch(() => caches.match("./index.html"));
+      return fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return resp;
+      }).catch(() => caches.match("./index.html"));
     })
   );
 });
